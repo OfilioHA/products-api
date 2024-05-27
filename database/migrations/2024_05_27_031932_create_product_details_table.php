4c4021adc04f6158ac807ed4b61f294e5019c559
OfilioHA <herdocia20@gmail.com>
2024-05-26 22:47:48 -0600

    feat: :sparkles: Adding migrations

---
+++ database/migrations/2024_05_27_031932_create_product_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained();
+            $table->string('image')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('product_detail_attributes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_detail_id')->constrained();
+            $table->foreignId('attribute_id')->constrained();
+            $table->string('value')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_details');
+    }
+};
