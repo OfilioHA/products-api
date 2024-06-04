38b6fb96a54db4294f9b8d30e6927e17a1ad61c4
OfilioHA <herdocia20@gmail.com>
2024-06-04 09:22:52 -0600

    Setting new routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,30 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::prefix('auth')->group(function () {
+        Route::get('/alive', [AuthController::class, 'alive']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/permissions', [AuthController::class, 'permissions']);
+    });
+
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('products', ProductController::class);
+});
+
+Route::prefix('/public')->group(function () {
+    Route::get('products', [ProductController::class, 'index']);
+    Route::get('products/{product}', [ProductController::class, 'show']);
+})->middleware([]);
