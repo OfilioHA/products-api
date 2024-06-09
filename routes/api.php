e8f8af81e82409376c3cb1ed2bdad71bba23d540
OfilioHA <herdocia20@gmail.com>
2024-06-09 10:48:03 -0600

    fix: :zap: FIxing routes

--- routes/api.php
+++ routes/api.php
@@ -6,24 +6,18 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('/auth/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
-
     Route::prefix('auth')->group(function () {
         Route::get('/alive', [AuthController::class, 'alive']);
         Route::post('/logout', [AuthController::class, 'logout']);
-        Route::post('/permissions', [AuthController::class, 'permissions']);
     });
 
     Route::apiResource('users', UserController::class);
-    Route::get('products/shopping-cart', [ProductController::class, 'shoppingCart']);
-    Route::get('products/wish-list', [ProductController::class, 'wishList']);
     Route::apiResource('products', ProductController::class);
+    Route::apiResource('products.shopping-cart', ProductController::class)->except(['update', 'show']);
+    Route::apiResource('products.wish-list', ProductController::class)->except(['update', 'show']);
 });
 
 Route::prefix('/public')->group(function () {
