3160bf9cb417c7292d7913408da699cdf49ce9a8
OfilioHA <herdocia20@gmail.com>
2024-06-09 10:45:41 -0600

    fix: :bug: Update migrations

--- database/migrations/2024_05_27_030839_create_categories_table.php
+++ database/migrations/2024_05_27_030839_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('image');
             $table->softDeletes();
         });
     }
@@ -23,7 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('product_categories');
         Schema::dropIfExists('categories');
     }
 };
